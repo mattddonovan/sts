839d2208da679dcfe2f2cf6e9ee04d57b0602cdf
mattddonovan <mattddonovan@gmail.com>
2018-11-07 00:26:42 -0600

    Some final CSS tweaks

--- src/components/SlideFeature.js
+++ src/components/SlideFeature.js
@@ -2,20 +2,35 @@ import React from 'react';
 
 const SlideFeature = ({ posts }) => {
   
+  // grab post objects
   const postLeft  = posts[0];
   const postRight = posts[1];
+
+  // 'inline' styles to dynamically populate background images
+  const postLeftBgStyle = {
+    backgroundImage: 'url("' + postLeft.imageUrl + '")',
+    backgroundPosition: 'left top',
+    backgroundSize: 'cover'
+  }
+  const postRightBgStyle = {
+    backgroundImage: 'url("' + postRight.imageUrl + '")',
+    backgroundPosition: 'right bottom',
+    backgroundSize: 'cover'
+  }
   
   return (
     <div className="container">
-      <div className="panel panel-left">
-        <img src={ postLeft.imageUrl } />
-        <p>{postLeft.title}</p>
-        <p>{ postLeft.subtitle }</p>
+      <div className="panel panel-left" style={ postLeftBgStyle }>
+        <div className="panel-text">
+          <h2><a href={ postLeft.linkUrl }>{ postLeft.title }</a></h2>
+          <p>{ postLeft.subtitle }</p>
+        </div>
       </div>
-      <div className="panel panel-right">
-        <img src={ postRight.imageUrl } />
-        <h2>{ postRight.title }</h2>
-        <p>{ postRight.subtitle }</p>
+      <div className="panel panel-right" style={ postRightBgStyle }>
+        <div className="panel-text">
+          <h2><a href={ postRight.linkUrl }>{ postRight.title }</a></h2>
+          <p>{ postRight.subtitle }</p>
+        </div>
       </div>
     </div>
   );
